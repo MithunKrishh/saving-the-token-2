aaf3f8b1954560f1d547c3515b45674dd96a0c60
MithunKrishh <mithunkannan06@gmail.com>
2025-04-07 10:28:11 +0530

    commited

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// script.js
+const jwt = require('jsonwebtoken');
+/**
+ * Generates a JWT with an expiry time.
+ * @param {Object} payload - The data you want to include in the token.
+ * @param {string} secret - Your secret key to sign the token.
+ * @returns {string} - The generated JWT.
+ */
 const encrypt = (payload, secret) => {
-  // your code here and return token
+  // Create and return the token with an expiration time (e.g., 1 hour)
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+  return token;
 };
 
 module.exports = encrypt;
